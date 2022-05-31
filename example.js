15c90294aadd7ea61287686309b05b3c37a739c0
Michael <michael@realt.co>
2022-05-31 15:21:55 +0200

    update to ES6

--- example.js
+++ example.js
@@ -4,59 +4,58 @@
  * @author Mohammad Fares <faressoft.com@gmail.com>
  */
 
-var inquirer = require('inquirer');
-var fuzzy = require('fuzzy');
+import inquirer from 'inquirer';
+const { registerPrompt, prompt } = inquirer;
 
-inquirer.registerPrompt('checkbox-plus', require('./index'));
+import fuzzy from 'fuzzy';
+
+import CheckboxPlusPrompt from './index.js'
+
+registerPrompt('checkbox-plus', CheckboxPlusPrompt);
 
 var colors = [
-  {name: 'The red color', value: 'red', short: 'red', disabled: false},
-  {name: 'The blue color', value: 'blue', short: 'blue', disabled: true},
-  {name: 'The green color', value: 'green', short: 'green', disabled: false},
-  {name: 'The yellow color', value: 'yellow', short: 'yellow', disabled: false},
-  {name: 'The black color', value: {name: 'black'}, short: 'black', disabled: false}
+  { name: 'The red color', value: 'red', short: 'red', disabled: false },
+  { name: 'The blue color', value: 'blue', short: 'blue', disabled: true },
+  { name: 'The green color', value: 'green', short: 'green', disabled: false },
+  { name: 'The yellow color', value: 'yellow', short: 'yellow', disabled: false },
+  { name: 'The black color', value: { name: 'black' }, short: 'black', disabled: false }
 ];
 
-inquirer.prompt([{
-  type: 'checkbox-plus',
-  name: 'colors',
-  message: 'Enter colors',
-  pageSize: 10,
-  highlight: true,
-  searchable: true,
-  default: ['yellow', 'red', {name: 'black'}],
-  validate: function(answer) {
+prompt([
+  {
+    type: 'checkbox-plus',
+    message: 'Enter colors',
+    name: 'colors',
+    pageSize: 10,
+    highlight: true,
+    searchable: true,
+    default: ['yellow', 'red', { name: 'black' }],
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return 'You must choose at least one topping.';
+      }
+      return true;
+    },
+    source: function (answersSoFar, input) {
+      input = input || '';
 
-    if (answer.length == 0) {
-      return 'You must choose at least one color.';
+      const result = new Promise(function (resolve) {
+        var fuzzyResult = fuzzy.filter(input, colors, {
+          extract: function (item) {
+            return item['name'];
+          }
+        });
+        var data = fuzzyResult.map(function (element) {
+          return element.original;
+        });
+        resolve(colors);
+      });
+      return result
+
     }
 
-    return true;
-
   },
-  source: function(answersSoFar, input) {
-
-    input = input || '';
-
-    return new Promise(function(resolve) {
-
-      var fuzzyResult = fuzzy.filter(input, colors, {
-        extract: function(item) {
-          return item['name'];
-        }
-      });
-
-      var data = fuzzyResult.map(function(element) {
-        return element.original;
-      });
-
-      resolve(data);
-      
-    });
-
-  }
-}]).then(function(answers) {
-
-  console.log(answers.colors);
-
-});
+])
+  .then(function (answers) {
+    console.log(answers.colors);
+  });
